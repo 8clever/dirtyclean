6dc6c3d3ea5b60200d8e492d3ec5871f69d63268
Ivan <godofluck89@gmail.com>
2020-09-17 23:59:59 +0300

    create timeline create play/pause by button in timeline add cutscene2

--- Assets/Scripts/Utils/CutSceneManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class CutSceneManager : MonoBehaviour
-{
-    void OnEndCutScene () {
-        SceneManager.LoadScene(Scenes.lvl1.ToString());
-    }
-}
