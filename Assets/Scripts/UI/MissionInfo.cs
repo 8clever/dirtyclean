89c40e78cfd0750be7bf6eb86fd0d15a7c2e2f47
Ivan <godofluck89@gmail.com>
2020-09-29 20:17:26 +0300

    add textMeshPro

--- Assets/Scripts/UI/MissionInfo.cs
+++ Assets/Scripts/UI/MissionInfo.cs
@@ -4,10 +4,13 @@ public class MissionInfo : MonoBehaviour {
     public Sprite Checked;
     public Sprite NotChecked;
     public Image Check;
-    public Text Text;
+    public TMPro.TMP_Text Text;
     public Mission mission;
     private void Start () {
         Check.sprite = mission.IsComplete() ? Checked : NotChecked;
         Text.text = mission.GetMissionInfo();
-    }   
+        if (mission.IsComplete()) {
+            Text.fontStyle = TMPro.FontStyles.Bold | TMPro.FontStyles.Strikethrough;
+        }
+    }
 }
