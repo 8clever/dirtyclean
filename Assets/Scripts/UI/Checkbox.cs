144a4a6a4aab0bbdb121bc4ee9d8e9201d4cbaf9
Ivan <godofluck89@gmail.com>
2020-10-16 23:45:43 +0300

    dirtyclean-92 Пустая ячейка без галочки больше по размеру чем с галочкой

--- Assets/Scripts/UI/Checkbox.cs
+++ Assets/Scripts/UI/Checkbox.cs
@@ -3,15 +3,15 @@ using UnityEngine.UI;
 
 [RequireComponent(typeof(Button))]
 public class Checkbox : MonoBehaviour {
-    public Sprite CheckedSprite;
-    public Sprite NotCheckedSprite;
-    public Image Check;
+    public GameObject GO_Checked;
+    public GameObject GO_NotChecked;
     public TMPro.TMP_Text Text;
     public string text;
     public bool Checked;
     public bool Underline = true;
     public void Update () {
-        Check.sprite = Checked ? CheckedSprite : NotCheckedSprite;
+        GO_Checked.SetActive(Checked);
+        GO_NotChecked.SetActive(!Checked);
         Text.text = text;
         if (Checked && Underline) {
             Text.fontStyle = TMPro.FontStyles.Bold | TMPro.FontStyles.Strikethrough;
